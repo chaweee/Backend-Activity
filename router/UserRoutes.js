6e89f3888e190921d79f6294fdc09564213316a5
chaweee <23-09852@g.batstate-u.edu.ph>
2025-11-24 19:33:31 +0800

    Fix import syntax and uncomment route handlers

--- router/UserRoutes.js
+++ router/UserRoutes.js
@@ -1,12 +1,11 @@
-import * as UserController from '../controllers/UserController.js';
-import express from 'express';
+import * as UserController from "../controllers/UserController.js";
+import express from "express";
+// import authHandler from '../middleware/authHandler.js';
 
 const UserRoutes = express.Router();
 
-UserRoutes.post('/register', 
-UserController.register); 
-UserRoutes.post('/login', 
-UserController.login); 
-
+// UserRoutes.use(authHandler);
+UserRoutes.post('/register', UserController.register);
+UserRoutes.post('/login', UserController.login);
 
 export default UserRoutes;
